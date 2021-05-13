6237dacbe1869b86ae0e52d728b7b42c42fc5ad2
Magesh Sampath <37866769+98MageshSampath@users.noreply.github.com>
2021-05-13 20:25:05 +0530

    Add files via upload

---
+++ tenstandard.java
@@ -0,0 +1,31 @@
+package Classroom;
+
+import 
+
+public class tenstandard {
+	
+    public void mathsClass(){
+    	
+    }
+    public void scienceclass() {	
+	  
+		  
+		  System.out.println("Attend Maths Class");
+		  System.out.println("Attend Science Class");
+	
+	  }
+   
+      public static void main(String[] Args) {
+    	  
+    	  //classname objectname = new classname 
+    	  tenstandard myClass = new tenstandard();
+    	  tenstandard myteacher = new tenstandard();
+    	  
+    	  
+    	  //objectname.methodname();
+    	  myClass.mathsClass();
+    	  myClass.scienceclass();
+    	  
+      }
+    	  
+      }
